def5ee33c280763aabef69c1671478b7da00e213
MCG <000914m@gmail.com>
2025-05-19 09:31:13 +0900

    feat: early stopping

--- IQL/online_train_6.py
+++ IQL/online_train_6.py
@@ -34,7 +34,7 @@ def main(args):
     now_str = datetime.now().strftime('%Y%m%d_%H%M')
     eval_log_path = os.path.join(args.eval_log_path, now_str)
 
-    obs_dim = args.obs_dim  # [T1, T2, TSP1, TSP2, prevQ1, prevQ2, dT1, dT2]
+    obs_dim = args.obs_dim
     act_dim = 2
     buffer = ReplayBuffer()
 
@@ -93,14 +93,15 @@ def main(args):
     epi_num, epi_step, epi_reward, done = args.n_eval_episodes-1, 0, 0, 1.0
     result,all_data= eval_policy()
     best_return = result['return mean']
+    patience = args.patience            # 조기 중단 대기 횟수
+    min_delta = 1        # 최소 변화량
+    no_improvement_steps = 0 # 개선되지 않은 스텝 카운트
     save_csv_png(all_data,0)
     
     initial_exp_prob = args.exp_prob  # 시작 확률
-    final_exp_prob = 0.05
+    final_exp_prob = 0.002
     for step in range(args.n_steps):
         if done:
-            #if epi_num > 0:
-                #log(f"episode: {epi_num} is done epi_reard: {epi_reward}")
             epi_num += 1
             epi_step = 0
             epi_reward = 0
@@ -108,11 +109,11 @@ def main(args):
             env.close()
             env = get_env(args.simmul)
             set_seed(epi_num)
-            #print(epi_num,"!!!!!!!!!!!!!!")
             Tsp1 = generate_random_tsp(args.max_episode_steps, 'TSP1')
             Tsp2 = generate_random_tsp(args.max_episode_steps, 'TSP2')
             set_seed(args.seed)
             exploration = False
+            exp_cnt=0
 
         
         if (epi_step + 1) % args.max_episode_steps == 0:
@@ -122,17 +123,22 @@ def main(args):
             cur_T1, cur_T2 = env.T1, env.T2
             prev_T1, prev_T2 = cur_T1, cur_T2
             prev_Q1, prev_Q2 = 29, 29
-            T1_buffer = [cur_T1] * 4
-            T2_buffer = [cur_T2] * 4
+            T1_buffer = [cur_T1] * 5
+            T2_buffer = [cur_T2] * 5
+            dT1 = 0.0
+            dT2 = 0.0
         else :
             cur_T1,cur_T2=next_T1,next_T2
+            dT1,dT2=next_dT1,next_dT2
         
-        if epi_step < 4:
+        '''if epi_step < 4:
             dT1 = cur_T1 - prev_T1
             dT2 = cur_T2 - prev_T2
         else:
             dT1 = cur_T1 - T1_buffer[0]
-            dT2 = cur_T2 - T2_buffer[0]
+            dT2 = cur_T2 - T2_buffer[0]'''
+            
+            
         
         #obs = np.array([cur_T1, cur_T2, Tsp1[epi_step], Tsp2[epi_step],prev_Q1,prev_Q2,dT1,dT2])
         obs = np.array([cur_T1,Tsp1[epi_step],dT1,cur_T2,Tsp2[epi_step],dT2])
@@ -141,33 +147,43 @@ def main(args):
 
         decay_rate = 0.99
         if (step) % 1000 == 0:
-            # decay는 적용하지만 final_exp_prob 이하로는 내려가지 않음
-            exp_prob = max(initial_exp_prob * (decay_rate ** (step// 1000)), final_exp_prob) 
+            decay_step = max(0, step - args.min_buffer_size)
+            exp_prob = max(initial_exp_prob * (decay_rate ** (decay_step // 1000)), final_exp_prob) 
         
-        if not exploration:
+        if args.static_exp_prob == True:
+            with torch.no_grad():
+                action,_ = policy.act(torchify(obs), deterministic=False, exp_prob=args.exp_prob, noise=args.noise,sample=False)
+        else:
+            with torch.no_grad():
+                action,_ = policy.act(torchify(obs), deterministic=False, exp_prob=exp_prob, noise=args.noise, sample=False)
+        action = action.cpu().numpy()        
+        '''if len(buffer) < args.min_buffer_size:
+            with torch.no_grad():
+                action, _ = policy.act(torchify(obs), deterministic=True)
+                action = action.cpu().numpy()
+                
+        elif not exploration:
             if args.static_exp_prob == True and args.sample == False:
                 with torch.no_grad():
-                    action,exploration = policy.act(torchify(obs), deterministic=False, sample=False).cpu().numpy()
+                    action,exploration = policy.act(torchify(obs), deterministic=False, sample=False)
             elif args.static_exp_prob == True and args.sample == True:
                 with torch.no_grad():
-                    action,exploration = policy.act(torchify(obs), deterministic=False, sample=True).cpu().numpy()
+                    action,exploration = policy.act(torchify(obs), deterministic=False, sample=True)
             elif args.static_exp_prob == False and args.sample == False:
-                if exp_prob == final_exp_prob:
-                    with torch.no_grad():
-                        action,exploration = policy.act(torchify(obs), deterministic=False, exp_prob=exp_prob, sample=True).cpu().numpy()
-                else:
-                    with torch.no_grad():
-                        action,exploration = policy.act(torchify(obs), deterministic=False, exp_prob=exp_prob, sample=False).cpu().numpy()
+                with torch.no_grad():
+                    action,exploration = policy.act(torchify(obs), deterministic=False, exp_prob=exp_prob, sample=False)
             elif args.static_exp_prob == False and args.sample == True:
                 with torch.no_grad():
-                    action,exploration = policy.act(torchify(obs), deterministic=False,exp_prob=exp_prob, sample=True).cpu().numpy()
-            exp_cnt=0
+                    action,exploration = policy.act(torchify(obs), deterministic=False,exp_prob=exp_prob, sample=True)
+            action = action.cpu().numpy()
+            
         else:
             action = np.array([prev_Q1, prev_Q2])
             exp_cnt += 1
-            if exp_cnt > 4:
+            #print("step",step,":",action)
+            if exp_cnt > 3:
                 exploration = False
-                exp_cnt = 0
+                exp_cnt = 0'''
             
         Q1, Q2 = action
         env.Q1(Q1)
@@ -187,44 +203,52 @@ def main(args):
         #print(f"epi:{epi_num} step:{epi_step} T1:{cur_T1} T2:{cur_T2} nT1:{next_T1} nT2:{next_T2} Q1:{Q1} Q2:{Q2}, Tsp1:{Tsp1[epi_step]}, Tsp2:{Tsp2[epi_step]}")
         if done:
             next_obs=obs
-            reward = 0.0 
+            reward = 0.0
+            next_obs = np.array([next_T1, Tsp1[epi_step-1], next_dT1, next_T2,Tsp2[epi_step-1],next_dT2])
         else:
             if epi_step < 4:
                 next_dT1 = next_T1 - cur_T1
                 next_dT2 = next_T2 - cur_T2
             else:
-                next_dT1 = next_T1 - T1_buffer[1]  # T1[t-3] == T1_buffer[1] (next_T는 아직 append 전이므로)
-                next_dT2 = next_T2 - T2_buffer[1]
-            #next_obs = np.array([next_T1, next_T2, Tsp1[epi_step], Tsp2[epi_step],Q1,Q2,next_dT1,next_dT2])
+                next_dT1 = next_T1 - T1_buffer[0] 
+                next_dT2 = next_T2 - T2_buffer[0]
             next_obs = np.array([next_T1, Tsp1[epi_step], next_dT1, next_T2,Tsp2[epi_step],next_dT2])
-            #next_obs = np.array([next_T1, Tsp1[epi_step], next_T2, Tsp2[epi_step]])
-            raw_reward = -np.linalg.norm([next_T1 - Tsp1[epi_step], next_T2 - Tsp2[epi_step]])
-            reward=normalize_reward(raw_reward, reward_scale=float(args.reward_scale))
-        #if epi_num == 8 and epi_step<300:
-            #print(f"epi:{epi_num} step:{epi_step} obs: {obs.round(3)} action:{action.round(3)}, next_obs:{next_obs.round(3)} reward: {reward:.3f}, "
-        #f"T1_buffer: {[round(v, 3) for v in T1_buffer]}, T2_buffer: {[round(v, 3) for v in T2_buffer]}")
+        #print(next_T1.shape, Tsp1[epi_step].shape, next_dT1.shape, next_T2.shape, Tsp2[epi_step].shape, next_dT2.shape)
+        
+
+        raw_reward = -np.linalg.norm([next_T1 - Tsp1[epi_step-1], next_T1 - Tsp2[epi_step-1]])
+        reward=normalize_reward(raw_reward, reward_scale=float(args.reward_scale))
 
         buffer.add((obs, action, next_obs, reward , done))
         epi_reward += reward
 
-        if len(buffer) > args.min_batch_size:
+        if len(buffer) > args.min_buffer_size:
             batch = buffer.sample(args.batch_size)
             #print(batch['next_observations'].shape) 
             iql.update(**batch)
-
+            #iql.only_policy_update(**batch)
             if (step + 1) % args.eval_period == 0:
                 result, all_data = eval_policy()
                 wandb.log({"step": step + 1})
+                current_return = result['return mean']
                 print(f"{step+1} step: {exp_prob:.4f} exp_prob")
-                if result['return mean'] > best_return:
+                if current_return > best_return + min_delta:
                     save_csv_png(all_data,step+1)
-                    best_return = result['return mean']
+                    best_return = current_return
                     best_path = log.dir / 'best.pt'
                     torch.save(iql.state_dict(), best_path)
                     print(f"📈 Best model saved with return {best_return:.2f}")
                     wandb.run.summary['best_return'] = best_return
                     wandb.save(str(best_path))
+                    no_improvement_steps = 0
+                else:
+                    no_improvement_steps += 1
+                    print(f"No improvement for {no_improvement_steps} steps...")
 
+                # Early Stopping 조건 확인
+                if no_improvement_steps >= patience:
+                    print("⏹️ Early stopping triggered. Training stopped.")
+                    break
     torch.save(iql.state_dict(), log.dir / 'final.pt')
     wandb.save(str(log.dir / 'final.pt'))
     log.close()
@@ -248,7 +272,7 @@ if __name__ == '__main__':
     parser.add_argument('--n-steps', type=int, default=10**6)
     parser.add_argument('--batch-size', type=int, default=256)
     parser.add_argument('--obs-dim', type=int, default=6)
-    parser.add_argument('--min-batch-size', type=int, default=54900)
+    parser.add_argument('--min-buffer-size', type=int, default=24000)
     parser.add_argument('--learning-rate', type=float, default=3e-4)
     parser.add_argument('--alpha', type=float, default=0.005)
     parser.add_argument('--tau', type=float, default=0.7)
@@ -260,8 +284,9 @@ if __name__ == '__main__':
     parser.add_argument('--reward-scale',default=10.0)
     parser.add_argument('--max-episode-steps', type=int, default=1000)
     parser.add_argument('--noise', type=float, default=3.5)
-    parser.add_argument('--exp-prob', type=float, default=0.25)
+    parser.add_argument('--exp-prob', type=float, default=0.3)
     parser.add_argument('--sample', default=False)
-    parser.add_argument('--static-exp-prob', default=True)
+    parser.add_argument('--static-exp-prob', default=False)
+    parser.add_argument('--patience', type=int, default=30)
 
     main(parser.parse_args())
